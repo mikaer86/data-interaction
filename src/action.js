a4ee5eb070e1e569ceb8eb285ab38c4f6bd9fc55
mikaer86 <mikaeleriksson123@gmail.com>
2023-04-04 16:15:34 +0200

    update

--- src/action.js
+++ src/action.js
@@ -1,16 +1,21 @@
+const fetch = require('node-fetch');
 const core = require('@actions/core');
 const github = require('@actions/github');
+
 async function run() {
+
     
     const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
-    const octokit = github.getOctokit('GITHUB_TOKEN');
+    const TENOR_TOKEN = github.getInput('TENOR_TOKEN');
 
     const {context = {}} = github;
     const {pull_request} = context.payload;
 
+    const octokit = github.getOctokit('GITHUB_TOKEN');
+
     await octokit.issues.createComment ({
         ...context.repo,
-        issue_number: pull_request_number,
+        issue_number: pull_request.number,
         body: "Thank you for submitting your pull request!"
 
     });
